aabd1825b5f403d2da79c888c44f2e37d3e13636
Mārtiņš Ciekurs <mciekurs@outlook.com>
2018-05-23 18:08:04 +0300

    Added analytics

--- app/src/main/java/com/mciekurs/prakt1/MainActivity.kt
+++ app/src/main/java/com/mciekurs/prakt1/MainActivity.kt
@@ -3,6 +3,7 @@ package com.mciekurs.prakt1
 import android.graphics.Bitmap
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
 import com.mciekurs.prakt1.fragments.FragmentAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -16,6 +17,12 @@ class MainActivity : AppCompatActivity() {
         pager.adapter = adapter
 
         tabs_main.setupWithViewPager(pager)
+
+        /*Firebase Analytics*/
+        val analytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "launched application")
+        analytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
 
     }
 
